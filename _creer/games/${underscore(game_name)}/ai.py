5fbdba78ec9f5b5f77e07f12c677c23046e7673d
Jacob Fischer <jacob.t.fischer@gmail.com>
2015-09-21 19:41:31 -0500

    AI functions can be void

--- _creer/games/${underscore(game_name)}/ai.py
+++ _creer/games/${underscore(game_name)}/ai.py
@@ -64,8 +64,8 @@ ${merge("        # ", "end", "        # replace with your end logic")}
 ${merge("        # ", function_name,
 """        # Put your game logic here for {0}
         return {1}
-""".format(function_name, shared['py']['default'](function_parms['returns']['type'], function_parms['returns']['default'])))
-}
+""".format(function_name, shared['py']['default'](function_parms['returns']['type'], function_parms['returns']['default']) if function_parms['returns'] else "")
+)}
 % endfor
 
 
