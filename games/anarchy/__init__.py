0fde3d1ad35af82f2d35520fe6893e5ac1e3c72f
Jacob Fischer <jacob.t.fischer@gmail.com>
2015-10-09 22:46:33 -0500

    Added Anarchy codegen'd files

---
+++ games/anarchy/__init__.py
@@ -0,0 +1,14 @@
+from games.anarchy.ai import AI
+from games.anarchy.game import Game
+from games.anarchy.building import Building
+from games.anarchy.fire_department import FireDepartment
+from games.anarchy.forecast import Forecast
+from games.anarchy.game_object import GameObject
+from games.anarchy.player import Player
+from games.anarchy.police_department import PoliceDepartment
+from games.anarchy.warehouse import Warehouse
+from games.anarchy.weather_station import WeatherStation
+
+# <<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+# if you need to initialize this module with custom logic do so here
+# <<-- /Creer-Merge: init -->>
