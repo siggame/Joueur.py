cb673e269ed2a2cf19ea8f3da1073f93e1c0f38b
Jacob Fischer <jacob.t.fischer@gmail.com>
2015-11-23 15:38:45 -0600

    Initial chess generation

---
+++ games/chess/__init__.py
@@ -0,0 +1,9 @@
+from games.chess.ai import AI
+from games.chess.game import Game
+from games.chess.game_object import GameObject
+from games.chess.piece import Piece
+from games.chess.player import Player
+
+# <<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+# if you need to initialize this module with custom logic do so here
+# <<-- /Creer-Merge: init -->>
