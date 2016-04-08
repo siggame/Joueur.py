6aa4afaeedc4a7e52e68fd113fedd55031bef2e4
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2016-04-08 10:08:30 -0400

    Add ShellAI Logic
    At the time of this push, there is still some oddness with the .busy
    property, which can be either a bool or a string. The if
    (spiderling.busy) clause reflects the uncertainty.

--- games/spiders/ai.py
+++ games/spiders/ai.py
@@ -3,6 +3,8 @@
 
 from joueur.base_ai import BaseAI
 
+import random
+
 class AI(BaseAI):
     """ The basic AI functions that are the same between games. """
 
@@ -44,4 +46,77 @@ class AI(BaseAI):
         Returns:
             bool: Represents if you want to end your turn. True means end your turn, False means to keep your turn going and re-call this function.
         """
+        # This is ShellAI, it is very simple, and demonstrates how to use all
+        # the game objects in Spiders.
+        spider = random.choice(self.player.spiders)
+
+        if spider.game_object_name == "BroodMother":
+            brood_mother = spider
+            choice = random.randint(0, 1)
+
+            if choice == 1: # try to consume a spiderling
+                if len(brood_mother.nest.spiders) > 1:
+                    otherSpider = random.choice(spider.nest.spiders)
+                    #if otherSpider != brood_mother:
+                        #print("BroodMother #" + brood_mother.id +
+                        #      " consuming " + otherSpider.game_object_name +
+                        #      " #" + otherSpider.id)
+                        #brood_mother.consume(otherSpider)
+            else: # try to spawn a Spiderling
+                if brood_mother.eggs > 0:
+                    # get a random spiderling type to spawn a new
+                    # Spiderling of that type
+                    randomSpiderlingType = random.choice(["Cutter", "Weaver", "Spitter"])
+                    print("BroodMother #" + brood_mother.id +
+                          " spawning " + randomSpiderlingType)
+                    brood_mother.spawn(randomSpiderlingType)
+        else: # it is a Spiderling
+            spiderling = spider
+
+            if spiderling.busy in [False, ""]:
+                choice = random.randint(0, 2)
+
+                if choice == 0: # try to move somewhere
+                    if len(spiderling.nest.webs) > 0:
+                        web = random.choice(spiderling.nest.webs)
+                        print("Spiderling " + spiderling.game_object_name +
+                              " #" + spiderling.id + " moving on Web #" + web.id)
+                elif choice == 1: # try to attack something
+                    if len(spiderling.nest.spiders) > 1:
+                        otherSpider = random.choice(spiderling.nest.spiders)
+                        if otherSpider.owner != spiderling.owner: # attack the enemy!
+                            spiderling.attack(otherSpider)
+                else: # do something unique based on Spiderling type
+                    if spiderling.game_object_name == "Spitter":
+                        spitter = spiderling
+                        enemysNest = self.player.other_player.brood_mother.nest
+
+                        # look for an existing web to the enemy home nest
+                        webExists = any(True
+                                        for web in enemysNest.webs
+                                        if spitter.nest in (web.nest_a, web.nest_b))
+
+                        if not webExists:
+                            print("Spitter #" + spitter.id +
+                                  " spitting to Nest #" + enemysNest.id)
+                            spitter.spit(enemysNest)
+                    elif spiderling.game_object_name == "Cutter":
+                        cutter = spiderling
+                        if len(cutter.nest.webs) > 0:
+                            web = random.choice(cutter.nest.webs)
+                            print("Cutter #" + cutter.id +
+                                  " cutting Web #" + web.id)
+                            cutter.cut(web)
+                    elif spiderling.game_object_name == "Weaver":
+                        weaver = spiderling
+                        if len(weaver.nest.webs) > 0:
+                            web = random.choice(weaver.nest.webs)
+                            if random.randint(0,1) == 1:
+                                print("Weaver #" + weaver.id +
+                                      " strengthening Web #" + web.id)
+                                weaver.strengthen(web)
+                            else:
+                                print("Weaver #" + weaver.id +
+                                      " weakening Web #" + web.id)
+                                weaver.weaken(web)
         return True
