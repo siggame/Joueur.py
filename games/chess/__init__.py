70d78827df3400df9be54fd5e4fe775f4f3ce1cb
Jonathan Henderson <jkhenderson999@gmail.com>
2019-11-15 19:02:32 -0600

    Deleted other games

--- games/chess/__init__.py
+++
@@ -1,9 +0,0 @@
-# DO NOT MODIFY THESE IMPORTS
-from games.chess.ai import AI
-from games.chess.game import Game
-from games.chess.game_object import GameObject
-from games.chess.player import Player
-
-# <<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-# if you need to initialize this module with custom logic do so here
-# <<-- /Creer-Merge: init -->>
