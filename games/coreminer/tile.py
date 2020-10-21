8fe1b129027d5928781c26ef548fff81159040ee
Jonathan Henderson <jkhenderson999@gmail.com>
2020-10-20 21:28:44 -0500

    add pathable logic

--- games/coreminer/tile.py
+++ games/coreminer/tile.py
@@ -201,7 +201,7 @@ class Tile(GameObject):
             bool: True if pathable, False otherwise
         """
         # <<-- Creer-Merge: is_pathable_builtin -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-        return false  # DEVELOPER ADD LOGIC HERE
+        return self.ore + self.dirt <= 0
         # <<-- /Creer-Merge: is_pathable_builtin -->>
 
     def has_neighbor(self, tile):
