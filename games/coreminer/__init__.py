dffa9bc31c08a93a7c61a8715a0a10c7f9b99f20
Jonathan Henderson <jkhenderson999@gmail.com>
2020-11-07 08:43:05 -0600

    Update timeAddedPerTurn

--- games/coreminer/__init__.py
+++ games/coreminer/__init__.py
@@ -8,7 +8,7 @@ from games.coreminer.player import Player
 from games.coreminer.tile import Tile
 from games.coreminer.upgrade import Upgrade
 
-game_version = 'a4592bb5acb0415146605769f439a09baf3768f41cdb3c7ad9dc971f42c4d96e'
+game_version = 'b559778acd8e4c689b8d028ca6cc154714ce79c39b09cd6d171b50faf88ef747'
 
 # <<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
 # if you need to initialize this module with custom logic do so here
